f6c71a1bff31fd400bea7900876c52e59e237253
lostmart <lostmart@hotmail.com>
2024-10-10 10:02:28 +0200

    on y vas

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("test", Math.random());
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.json())
+
+// GET: LIRE  tout les utilizateurs
+app.get("/", (req, res) => {
+	res.json({
+		msg: "hello rest api 🎉",
+	})
+})
+
+// POST: CREER un utiilizateur avec les donnes du body
+app.post("/", (req, res) => {
+	res.json({
+		msg: "hello rest api ici le POST !!!! ",
+	})
+})
+
+// PUT: MODIFIER un utiilizateur avec les donnes du body
+app.put("/", (req, res) => {
+	res.json({
+		msg: "hello rest api ici le PUT ",
+	})
+})
+
+// DELETE: effacer un utilizateur
+app.delete("/", (req, res) => {
+	res.json({
+		msg: "hello rest api ici le DELETE ",
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`)
+})
